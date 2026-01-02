6f35776936c61ec35e9d74ec41b545fd8a6fa954
maafik <maafik@66.ru>
2026-01-02 23:01:30 +0300

    3212

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,10 +68,10 @@
     window.addEventListener('load', () => {
       setTimeout(() => {
         preloader.classList.add('loaded');
-      }, 1000);
+      }, 300);
       setTimeout(() => {
         preloader.remove();
-      }, 2000);
+      }, 800);
     });
   }
 
